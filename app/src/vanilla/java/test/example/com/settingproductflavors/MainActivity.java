d0ed62eaa7279e7562fd7c35368409ae87bedb9d
son vo <ngocson160391@gmail.com>
2016-05-15 23:42:46 +0700

    Push code

---
+++ app/src/vanilla/java/test/example/com/settingproductflavors/MainActivity.java
@@ -0,0 +1,20 @@
+package test.example.com.settingproductflavors;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.text) TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        text.setText("vanilla:   222222222222222222222222222222");
+    }
+}
